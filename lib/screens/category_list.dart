8b036387c32bcf585902f2c07d12d7a688245208
Isabella <sorianoisabella28@gmail.com>
2023-10-30 13:04:56 -0500

    vista de categorylist de imagen y nombre

--- lib/screens/category_list.dart
+++ lib/screens/category_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+import 'package:meals_app/services/category_service.dart';
 
 class CategoryList extends StatefulWidget {
   const CategoryList({super.key});
@@ -9,10 +11,69 @@ class CategoryList extends StatefulWidget {
 }
 
 class _CategoryListState extends State<CategoryList> {
+
+  CategoryService? _categoryService;
+  List? _categories;
+
+  initialize()async{
+    //llama al metodo del servicio
+    _categories=await _categoryService?.getAll() ??[];
+    setState(() { //lo renderiza
+      _categories=_categories;
+    });
+  }
+
+  @override
+  void initState(){
+    _categoryService=CategoryService(); //inicializa el servicio
+    initialize();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text("Esta es la vista de category list"),
+    return ListView.builder(
+      itemCount:_categories?.length??0, //asigna el tamaño, sino hay le coloca 0
+      itemBuilder: (context, index){
+        return CategoryItem(category: _categories?[index]);
+      }
     );
   }
 }
+
+class CategoryItem extends StatefulWidget {
+  const CategoryItem({super.key, required this.category});
+
+  //parametros
+  final Category category;
+
+  @override
+  State<CategoryItem> createState() => _CategoryItemState();
+}
+
+class _CategoryItemState extends State<CategoryItem> {
+  @override
+  Widget build(BuildContext context) {
+
+    //defino la imagen
+    final image=Image.network(widget.category.imageUrl);
+
+    return Card(
+      child: Column(
+        children: [
+          image,
+          Text(widget.category.name),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
